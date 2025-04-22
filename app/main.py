b387d479081c9ab934511f73de33fad6fd40452c
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-04-22 19:10:56 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    
+    connection, address = server_socket.accept() # wait for client
+
+    with connection:
+        print(f"Connected by {address}")
+        connection.sendall(b"HTTP/1.1 200 OK\r\n")
+        connection.close()
+
 
 
 if __name__ == "__main__":
