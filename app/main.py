f62a9c6fb0419a54e3f3f173d2390cc5fb841d5c
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-04-23 18:06:09 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,13 +13,22 @@ def main():
     data = connection.recv(1024)
     request_data = data.decode().split("\r\n")
     request_line = request_data[0]
+    print(request_line)
     method, target, http_version = request_line.split()
+   
 
     if target == "/":
         response = b"HTTP/1.1 200 OK\r\n\r\n"
     elif target.startswith("/echo/"):
         value = target.split("/echo/")[1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
+    elif target =="/user-agent":
+        request_line2 = request_data[2]
+        print(request_line2)
+        header, value1 = request_line2.split(":", 1)  
+        header = header.strip() 
+        value1 = value1.strip()
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value1)}\r\n\r\n{value1}".encode()
     else:
         response = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
