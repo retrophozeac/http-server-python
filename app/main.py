ec8386f6b8e2495eb95f2345e0a1fca7be6d99ca
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-04-23 20:32:31 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,31 +9,41 @@ def handle_client(connection):
         request_line = request_data[0]
         print(request_line)
         method, target, http_version = request_line.split()
-
-        if target == "/":
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-        elif target.startswith("/echo/"):
-            value = target.split("/echo/")[1]
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
-        elif target == "/user-agent":
-            request_line2 = request_data[2]
-            print(request_line2)
-            header, value1 = request_line2.split(":", 1)
-            header = header.strip()
-            value1 = value1.strip()
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value1)}\r\n\r\n{value1}".encode()
-        elif target.startswith("/files/"):
-            filename = target.split("/files/")[1]
-            directory = sys.argv[2]
-            print(filename)
-            try:
-                with open(f"/{directory}/{filename}", "r") as file:
-                    content = file.read()
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}".encode()
-            except FileNotFoundError:
+        if method == "GET":
+            if target == "/":
+                response = b"HTTP/1.1 200 OK\r\n\r\n"
+            elif target.startswith("/echo/"):
+                value = target.split("/echo/")[1]
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
+            elif target == "/user-agent":
+                request_line2 = request_data[2]
+                print(request_line2)
+                header, value1 = request_line2.split(":", 1)
+                header = header.strip()
+                value1 = value1.strip()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value1)}\r\n\r\n{value1}".encode()
+            elif target.startswith("/files/"):
+                filename = target.split("/files/")[1]
+                directory = sys.argv[2]
+                print(filename)
+                try:
+                    with open(f"/{directory}/{filename}", "r") as file:
+                        content = file.read()
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}".encode()
+                except FileNotFoundError:
+                    response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+            else:
                 response = b"HTTP/1.1 404 Not Found\r\n\r\n"
-        else:
-            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        elif method == "POST":
+            if target.startswith("/files/"):
+                filename = target.split("/files/")[1]
+                directory = sys.argv[2]
+                body = request_data[-1]
+                with open(filename, "w") as file:
+                    file.write(body.decode())
+                response = f"HTTP/1.1 201 Created\r\n\r\n"
+            else:
+                response = b"HTTP/1.1 404 Not Found\r\n\r\n"    
 
         connection.sendall(response)
     finally:
